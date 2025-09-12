354881e764a55d1a7755cc6cd9c6c9e40452a2e3
Adhithya Narendran Sundar <x23421215@student.ncirl.ie>
2025-09-12 09:13:43 +0100

    newpush

--- src/tests/locust/locustfile.py
+++ src/tests/locust/locustfile.py
@@ -1,5 +1,6 @@
 from locust import HttpUser, task, between, events
 from prometheus_client import start_http_server, Counter, Histogram
+import os
 
 # Prometheus metrics
 REQUEST_COUNT = Counter(
@@ -21,6 +22,9 @@ def track_request(request_type, name, response_time, response_length, exception,
     REQUEST_COUNT.labels(request_type, name, status).inc()
     REQUEST_LATENCY.labels(request_type, name).observe(response_time / 1000.0)
 
+# Read env var (default = true for local, false in CI/CD)
+NEGATIVE_TESTS = os.getenv("NEGATIVE_TESTS", "true").lower() == "true"
+
 class CrudApiUser(HttpUser):
     wait_time = between(1, 3)
 
@@ -28,9 +32,10 @@ class CrudApiUser(HttpUser):
     def call_health(self):
         self.client.get("/")
 
-    @task
-    def call_invalid(self):
-        self.client.get("/this-does-not-exist")
+    if NEGATIVE_TESTS:
+        @task
+        def call_invalid(self):
+            self.client.get("/this-does-not-exist")
 
     @task
     def crud_cycle(self):
